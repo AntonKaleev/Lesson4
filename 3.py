8e63b71de93cd055e528fb5fa3a3c7d353cee924
Anton Kaleev <antonkaleev986@gmail.com>
2024-03-16 09:33:35 +0400

    4

--- 3.py
+++ 3.py
@@ -1,5 +1,8 @@
-from tkinter import *root = Tk()
+from tkinter import *
+root = Tk()
 def myClick():
-    myLabel = Label(root, text="Нажата кнопка!")    myLabel.pack()
-myButton = Button(root, text = "Нажмите", command=myClick, fg="blue", bg="#ffffff")myButton.pack()
+    myLabel = Label(root, text="Нажата кнопка!")
+    myLabel.pack()
+myButton = Button(root, text = "Нажмите", command=myClick, fg="blue", bg="#ffffff")
+myButton.pack()
 root.mainloop()
